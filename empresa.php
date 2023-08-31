a13cc8ddd371d6b30118c04f5ffa94834a09aff7
Sidnei Augusto de Miranda <sidnei.miranda@rede.local>
2023-08-31 10:29:09 -0300

    ajustes single de produtos

--- empresa.php
+++ empresa.php
@@ -100,76 +100,7 @@
         <div class="bg"><span>COMPROMISSO COM A QUALIDADE</span></div>
     </section>
 
-    <section id="slider-infos">
-        <div class="bg">
-                            </div>
-        <h1 class="hidden-xs">Nossos Produtos</h1>
-        <div class="container">
-            <div class="info-titulo-mobile hidden-lg">
-                <h2>Nossos Produtos</h2>
-                <img src="#">
-            </div>
-                                        <div class="info">
-                <img src="produtos/compressor-de-pintura-40lbs.jpg" alt="Background Info" class="bg">
-                <!-- <h3 class="hidden-xs">Compressor de Pintura 140Lbs</h3> -->
-                <div class="hover">
-                    <h3>COMPRESSOR DE PINTURA 40LBS</h3>
-                                        </div>
-            </div>
-                                        <div class="info">
-                <img src="produtos/motor-vibrador-eletrico.jpg" alt="Background Info" class="bg">
-                <!-- <h3 class="hidden-xs">Motor Vibrador a Gasolina</h3> -->
-                <div class="hover">
-                    <h3>MOTOR VIBRADOR ELÉTRICO</h3>
-                                        </div>
-            </div>
-                                        <div class="info">
-                <img src="produtos/esmerilhadeira-angular-4-1-2.jpg" alt="Background Info" class="bg">
-                <!-- <h3 class="hidden-xs">Compressor Pneumático 92 PCM</h3> -->
-                <div class="hover">
-                    <h3>ESMERILHADEIRA ANGULAR 4 1/2'</h3>
-                                        </div>
-            </div>
-                                        <div class="info">
-                <img src="produtos/serra-clipper-a-gasolina-ou-eletrica.jpg" alt="Background Info" class="bg">
-                <!-- <h3 class="hidden-xs">Serra Circular 7”</h3> -->
-                <div class="hover">
-                    <h3>SERRA CLIPPER A GASOLINA OU ELÉTRICA</h3>
-                                        </div>
-            </div>
-                                        <div class="info">
-                <img src="produtos/serra-tico-tico.jpg" alt="Background Info" class="bg">
-                <!-- <h3 class="hidden-xs">SERRA MARMORE</h3> -->
-                <div class="hover">
-                    <h3>SERRA TICO - TICO</h3>
-                                        </div>
-            </div>
-                                        <div class="info">
-                <img src="produtos/martelo-rompedor-10kg.jpg" alt="Background Info" class="bg">
-                <!-- <h3 class="hidden-xs">KIT FURADEIRA </h3> -->
-                <div class="hover">
-                    <h3>MARTELO ROMPEDOR 10KG</h3>
-                                        </div>
-            </div>
-                                        <div class="info">
-                <img src="produtos/bomba-lameira-4-polegadas.jpg" alt="Background Info" class="bg">
-                <!-- <h3 class="hidden-xs">Compressor de Pintura 175Lbs</h3> -->
-                <div class="hover">
-                    <h3>BOMBA LAMEIRA 4'</h3>
-                                        </div>
-            </div>
-                                        <div class="info">
-                <img src="produtos/guincho-de-coluna.jpg" alt="Background Info" class="bg">
-                <!-- <h3 class="hidden-xs">Misturador de Argamassa</h3> -->
-                <div class="hover">
-                    <h3>GUINCHO DE COLUNA</h3>
-                                        </div>
-            </div>
-                    <a class="hidden-lg" href="https://www.newlocequipamentos.com.br/produtos.php" target="_blank" rel="noopener noreferrer">Veja Mais</a>
-        </div>
-            <a class="hidden-xs" href="https://www.newlocequipamentos.com.br/produtos.php" target="_blank" rel="noopener noreferrer">Veja Mais</a>
-
-    </section>
+    <?php include('resumeProdutos');?>
 
     <section id="paralax" style="background-image: url('assets/paralax-scaled.jpg')"></section>
     <section id="compromisso">
--- produto.php
+++ produto.php
@@ -88,76 +88,7 @@
     
     <section id="sobreEvento"></section>
 
-    <section id="slider-infos">
-        <div class="bg">
-                            </div>
-        <h1 class="hidden-xs">Outros Produtos</h1>
-        <div class="container">
-            <div class="info-titulo-mobile hidden-lg">
-                <h2>Nossos Produtos</h2>
-                <img src="#">
-            </div>
-                                        <div class="info">
-                <img src="produtos/compressor-de-pintura-140lbs.jpg" alt="Background Info" class="bg">
-                <!-- <h3 class="hidden-xs">Compressor de Pintura 140Lbs</h3> -->
-                <div class="hover">
-                    <h3>Compressor de Pintura 140Lbs</h3>
-                                        </div>
-            </div>
-                                        <div class="info">
-                <img src="produtos/motor-vibrador-a-gasolina.jpg" alt="Background Info" class="bg">
-                <!-- <h3 class="hidden-xs">Motor Vibrador a Gasolina</h3> -->
-                <div class="hover">
-                    <h3>Motor Vibrador a Gasolina</h3>
-                                        </div>
-            </div>
-                                        <div class="info">
-                <img src="produtos/compressor-pneumatico-92-pcm.jpg" alt="Background Info" class="bg">
-                <!-- <h3 class="hidden-xs">Compressor Pneumático 92 PCM</h3> -->
-                <div class="hover">
-                    <h3>Compressor Pneumático 92 PCM</h3>
-                                        </div>
-            </div>
-                                        <div class="info">
-                <img src="produtos/serra-circular-7.jpg" alt="Background Info" class="bg">
-                <!-- <h3 class="hidden-xs">Serra Circular 7”</h3> -->
-                <div class="hover">
-                    <h3>Serra Circular 7”</h3>
-                                        </div>
-            </div>
-                                        <div class="info">
-                <img src="produtos/serra-marmore.jpg" alt="Background Info" class="bg">
-                <!-- <h3 class="hidden-xs">SERRA MARMORE</h3> -->
-                <div class="hover">
-                    <h3>SERRA MARMORE</h3>
-                                        </div>
-            </div>
-                                        <div class="info">
-                <img src="produtos/furadeira-e-parafusadeira-eletrica.jpg" alt="Background Info" class="bg">
-                <!-- <h3 class="hidden-xs">KIT FURADEIRA </h3> -->
-                <div class="hover">
-                    <h3>KIT FURADEIRA </h3>
-                                        </div>
-            </div>
-                                        <div class="info">
-                <img src="produtos/misturador-de-argamassa.jpg" alt="Background Info" class="bg">
-                <!-- <h3 class="hidden-xs">Compressor de Pintura 175Lbs</h3> -->
-                <div class="hover">
-                    <h3>Compressor de Pintura 175Lbs</h3>
-                                        </div>
-            </div>
-                                        <div class="info">
-                <img src="produtos/compressor-de-pintura-175lbs.jpg" alt="Background Info" class="bg">
-                <!-- <h3 class="hidden-xs">Misturador de Argamassa</h3> -->
-                <div class="hover">
-                    <h3>Misturador de Argamassa</h3>
-                                        </div>
-            </div>
-                    <a class="hidden-lg" href="#" target="_blank" rel="noopener noreferrer">Veja Mais</a>
-        </div>
-            <a class="hidden-xs" href="#" target="_blank" rel="noopener noreferrer">Veja Mais</a>
-
-    </section>
+    <?php include('resumeProdutos.php');?>
 
     <?php include('footer.html');?>
 
